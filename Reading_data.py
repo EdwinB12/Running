8b888533d9059a6d891c28b0681a35bc9c7a8e33
Edwin <w.edwin.brown@outlook.com>
2020-04-30 17:50:15 +0100

    Optimise Data Types
    Optimise data types for each column of dataframe and generally preparing for visualisation.

--- Reading_data.py
+++ Reading_data.py
@@ -27,5 +27,45 @@ def Initial_Data_QC(df):
     print(df.info(),'\n')
     print(df.describe(),'\n') #Only float columns are shown here
     
-#%% Remove unwanted characters from data columns    
-    
+#%% Prepare data in every column for visualising and ML    
+
+#Where data is missing, it is replaced with --  We will change these to 0s for the time being. 
+Run_data = Run_data.replace('--','0')
+
+#First try and optimise types of all columns
+Run_data = Run_data.convert_dtypes()
+
+#Calories Column - Remove any commas and convert to int
+Run_data['Calories'] = Run_data['Calories'].str.replace(',','')
+Run_data['Calories'] = Run_data['Calories'].astype(int)
+
+#Converting certain columns from string to int or float 
+Run_data['Avg HR'] = Run_data['Avg HR'].astype(int)
+Run_data['Max HR'] = Run_data['Max HR'].astype(int)
+Run_data['Aerobic TE'] = Run_data['Aerobic TE'].astype(float)
+Run_data['Avg Run Cadence'] = Run_data['Avg Run Cadence'].astype(int)
+Run_data['Max Run Cadence'] = Run_data['Max Run Cadence'].astype(int)
+Run_data['Elev Gain'] = Run_data['Elev Gain'].astype(int)
+Run_data['Elev Loss'] = Run_data['Elev Loss'].astype(int)
+
+#Removing ':' from Avg and Best pace and replacing with a '.' and change to float
+Run_data['Avg Pace'] = Run_data['Avg Pace'].str.replace(':','.')
+Run_data['Best Pace'] = Run_data['Best Pace'].str.replace(':','.')
+Run_data['Avg Pace'] = Run_data['Avg Pace'].astype(float)
+Run_data['Best Pace'] = Run_data['Best Pace'].astype(float)
+
+#Converting Date to Datetime: 
+Run_data['Date']= pd.to_datetime(Run_data['Date'],format = '%d/%m/%Y %H:%M')
+#Calculate decimal minutes from 'Time' and add new column called 'Time_dec_min'
+Run_data['Time_dec_min'] = Run_data['Time'].str.split(':').apply(lambda x: ((int(x[0]) * 3600) + (int(x[1])*60) + int(x[2])/60))
+
+print(Run_data.head())
+print(Run_data.dtypes)
+
+#%% Data Insights
+
+
+
+
+
+
