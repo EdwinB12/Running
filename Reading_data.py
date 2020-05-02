35b542af84e6ac7d51569c4288dd736e5edb2578
Edwin <w.edwin.brown@outlook.com>
2020-05-02 13:08:38 +0100

    Renamed Columns
    Renamed columns to not include spaces so '.' functionality could be used.

--- Reading_data.py
+++ Reading_data.py
@@ -18,7 +18,8 @@ df = pd.read_csv(path)
 
 #Hand chosen 15 columns of interest
 Run_data = df[['Distance', 'Date', 'Title', 'Calories','Time', 'Avg HR', 'Max HR', 'Aerobic TE', 'Avg Run Cadence', 'Max Run Cadence', 'Avg Pace', 'Best Pace', 'Elev Gain', 'Elev Loss', 'Avg Stride Length']]
-
+#Modifying and tidying 'Title' Label - Change column name to 'Location'. 
+Run_data.rename(columns={'Title': 'Location', 'Avg HR':'Avg_HR', 'Max HR':'Max_HR', 'Aerobic TE':'Aerobic_TE', 'Avg Run Cadence':'Avg_Run_Cadence','Max Run Cadence':'Max_Run_Cadence','Avg Pace':'Avg_Pace', 'Best Pace':'Best_Pace', 'Elev Gain':'Elev_Gain','Elev Loss':'Elev_Loss', 'Avg Stride Length':'Avg_Stride_Length'},inplace=True)
 
 #%% Initial Data QC
 def Initial_Data_QC(df): 
@@ -42,25 +43,24 @@ Run_data['Calories'] = Run_data['Calories'].str.replace(',','')
 Run_data['Calories'] = Run_data['Calories'].astype(int)
 
 #Converting certain columns from string to int or float 
-Run_data['Avg HR'] = Run_data['Avg HR'].astype(int)
-Run_data['Max HR'] = Run_data['Max HR'].astype(int)
-Run_data['Aerobic TE'] = Run_data['Aerobic TE'].astype(float)
-Run_data['Avg Run Cadence'] = Run_data['Avg Run Cadence'].astype(int)
-Run_data['Max Run Cadence'] = Run_data['Max Run Cadence'].astype(int)
-Run_data['Elev Gain'] = Run_data['Elev Gain'].astype(int)
-Run_data['Elev Loss'] = Run_data['Elev Loss'].astype(int)
+Run_data['Avg_HR'] = Run_data['Avg_HR'].astype(int)
+Run_data['Max_HR'] = Run_data['Max_HR'].astype(int)
+Run_data['Aerobic_TE'] = Run_data['Aerobic_TE'].astype(float)
+Run_data['Avg_Run_Cadence'] = Run_data['Avg_Run_Cadence'].astype(int)
+Run_data['Max_Run_Cadence'] = Run_data['Max_Run_Cadence'].astype(int)
+Run_data['Elev_Gain'] = Run_data['Elev_Gain'].astype(int)
+Run_data['Elev_Loss'] = Run_data['Elev_Loss'].astype(int)
 
 #Removing ':' from Avg and Best pace and replacing with a '.' and change to float
-Run_data['Avg Pace'] = Run_data['Avg Pace'].str.replace(':','.')
-Run_data['Best Pace'] = Run_data['Best Pace'].str.replace(':','.')
-Run_data['Avg Pace'] = Run_data['Avg Pace'].astype(float)
-Run_data['Best Pace'] = Run_data['Best Pace'].astype(float)
+Run_data['Avg_Pace'] = Run_data['Avg_Pace'].str.replace(':','.')
+Run_data['Best_Pace'] = Run_data['Best_Pace'].str.replace(':','.')
+Run_data['Avg_Pace'] = Run_data['Avg_Pace'].astype(float)
+Run_data['Best_Pace'] = Run_data['Best_Pace'].astype(float)
 
 #Saving off original 'Title' column before editing
-Run_data['Orig_Location'] = Run_data['Title']
-#Modifying and tidying 'Title' Label - Change column name to 'Location'. 
-Run_data.rename(columns={'Title': 'Location'},inplace=True)
-#Reduce categories, many are repeats with different spellings, as seen below
+Run_data['Orig_Location'] = Run_data['Location']
+
+#Reduce Location categories, many are repeats with different spellings, as seen below
 Orig_Locations = Run_data['Location'].value_counts() #18 categories
 #Collect all strings for Wokingham
 Run_data['Location'] = Run_data['Location'].str.replace('Wokingham Running','Wokingham')
@@ -107,13 +107,13 @@ Run_5k_less = Run_data.loc[(Run_data['Distance'] < 4.9)]
 Run_5k_more = Run_data.loc[(Run_data['Distance'] > 5.1)]
 
 #Create Seperate dataframes per Location (if needed)
-Run_5k_Wokingham = Run_5k.loc[(Run_data['Location'].str.match('Wokingham'))]
-Run_5k_Woodley = Run_5k.loc[(Run_data['Location'].str.match('Woodley'))]
-Run_5k_TVP = Run_5k.loc[(Run_data['Location'].str.match('TVP'))]
-
-#%% Data Insights
-
-
+Run_5k_Wokingham = Run_5k.loc[(Run_5k['Location'].str.match('Wokingham'))]
+Run_5k_Woodley = Run_5k.loc[(Run_5k['Location'].str.match('Woodley'))]
+Run_5k_TVP = Run_5k.loc[(Run_5k['Location'].str.match('TVP'))]
+Run_5k_Other =  Run_5k.loc[(Run_5k['Location'].str.match('Other'))]
+Run_5k_Nottingham =  Run_5k.loc[(Run_5k['Location'].str.match('Nottingham'))]
+Run_5k_Wirral =  Run_5k.loc[(Run_5k['Location'].str.match('Wirral'))]
+Run_5k_Erewash =  Run_5k.loc[(Run_5k['Location'].str.match('Erewash'))]
 
 
 
